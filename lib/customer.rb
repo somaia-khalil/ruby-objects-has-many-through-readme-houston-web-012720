0bc4910b70770e6081855f8c24d8f65c4e226de7
somaia-khalil <sumaya_khair@live.com>
2020-01-31 01:02:58 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,25 @@
 class Customer
-  
+    @@all = []
+    attr_accessor :name , :age
+  def initialize (name , age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_meal(waiter,total,tip)
+    Meal.new(waiter,self,total,tip)
+  end 
+  def meals
+    Meal.all.select do |meal|
+        meal.customer == self
+    end 
+  end
+  def waiters
+   meals.map do |meal|
+    meal.waiter
+   end 
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+    @@all = []
+    attr_accessor :name , :yoe
+    def initialize(name ,yoe)
+        @name = name
+        @yoe = yoe
+        @@all << self
+    end
+    def self.all
+        @@all
+    end 
+    def new_meal(customer,total,tip)
+        Meal.new(self , customer , total , tip)
+    end
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end 
+    def best_tipper
+        htip = meals.max do |a ,b|
+            a.tip <=> b.tip
+        end
+        htip.customer
+    end
+  
 
 end
